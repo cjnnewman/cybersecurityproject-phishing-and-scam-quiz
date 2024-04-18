d3c5616dcdcc269b883329de93106dbe7f5ffbf3
William Ward <wcward3302@eagle.fgcu.edu>
2024-04-17 21:57:23 -0400

    Welcome page content - Lentz Fortune

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,8 +13,8 @@ export const metadata: Metadata = {
 };
 
 export default async function RootLayout({
-                                           children,
-                                         }: {
+  children,
+}: {
   children: React.ReactNode;
 }) {
   const session = await getServerSession();
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,22 @@
 import Link from "next/link"
+import Image from "next/image"
 
 export default function Home() {
   return (
-    <main className="h-screen flex items-center justify-center">
+  <main>
+    <div>
+      <p>
+        According to the University of Michigan Institute for Healthcare Policy and Innovation, in a survey of people ages 50-80 years old, 75% of 
+        them reported having encountered an attempt at scamming them via phone, text, email, or mail. 30% reported that they were a victim of scams. 
+        This Assessment hopes to help members of the elderly population analyze their scam recognition skills.
+      </p>
       <div>
-        <p className="text-4xl font-black text-gray-900 dark:text-white">Test</p>
+        <Image className="image" src="/shocked_man_with_phone.jpeg" alt="image of a shocked man with phone in hand" width={500} height={1000} />
+        <Link href="/login-register">
+          <button className="button">Continue to Site!</button>
+        </Link>
       </div>
-    </main>
+    </div>
+  </main>
   )
 }
